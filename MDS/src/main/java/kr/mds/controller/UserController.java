e44f14cd5364963f776f4c216e461f7f7fff60cb
smhrd <smhrd@DESKTOP-D1TDC6O>
2023-07-11 14:41:53 +0900

    팀장 -ㅋㅁ

--- MDS/src/main/java/kr/mds/controller/UserController.java
+++ MDS/src/main/java/kr/mds/controller/UserController.java
@@ -1,8 +1,13 @@
 package kr.mds.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserController {
-
+	
+	@RequestMapping("/")
+	public String singUp() {
+		return "signUp";
+	}
 }
