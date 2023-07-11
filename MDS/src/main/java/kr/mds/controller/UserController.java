d3a8601c01fa580a083fa335cccc34863f2e7e94
smhrd <smhrd@DESKTOP-D1TDC6O>
2023-07-11 14:33:32 +0900

    팀장 - ㅋㅁ

---
+++ MDS/src/main/java/kr/mds/controller/UserController.java
@@ -0,0 +1,8 @@
+package kr.mds.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+
+}
---
+++ MDS/src/main/java/kr/mds/entity/User.java
@@ -0,0 +1,12 @@
+package kr.mds.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class User {
+	private int user_num;
+}
