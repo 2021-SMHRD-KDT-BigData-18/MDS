7e8878dce558e00d24675c5ee77ca29937e5eb27
smhrd <smhrd@DESKTOP-PKL3N2D>
2023-07-17 14:35:18 +0900

    로그아웃삭제 commit

--- MDS/src/main/java/kr/mds/controller/userController.java
+++ MDS/src/main/java/kr/mds/controller/userController.java
@@ -100,12 +100,10 @@ public class userController {
 		else
 			return "redirect:/signIn.com";
 	}
-		//로그아웃
-		@GetMapping("/")
-		public String logout() {
-		        
-		        return "redirect:/login.com";
+		
 		
 	}
 
-}
+		
+	
+
